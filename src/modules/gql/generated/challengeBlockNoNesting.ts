dfeca3d55ac5c670b5b17e937e863e7a62aad2c5
Fi1osof <n.lanets@modxclub.ru>
2020-12-26 00:02:24 +0300

    Fix lessons page

--- src/modules/gql/generated/challengeBlockNoNesting.ts
+++ src/modules/gql/generated/challengeBlockNoNesting.ts
@@ -9,15 +9,11 @@
 import * as Types from './types';
 
 import { gql } from '@apollo/client';
-export type ChallengeBlockNoNestingFragment = { __typename?: 'CodeChallengeBlock', id: string, externalKey?: Types.Maybe<string>, createdAt: Date, updatedAt: Date, name?: Types.Maybe<string>, rank?: Types.Maybe<number> };
+export type ChallengeBlockNoNestingFragment = { __typename?: 'CodeChallengeBlock', id: string, name?: Types.Maybe<string> };
 
 export const ChallengeBlockNoNestingFragmentDoc = gql`
     fragment challengeBlockNoNesting on CodeChallengeBlock {
   id
-  externalKey
-  createdAt
-  updatedAt
   name
-  rank
 }
     `;
